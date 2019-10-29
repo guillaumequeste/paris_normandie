6c2051ced430e176d315c2e09297a3570cdd6487
Guillaume Queste <guillaume.queste@laposte.net>
2019-10-29 10:24:48 +0100

    detail mobile

--- src/components/ALire/ALireDetail.js
+++ src/components/ALire/ALireDetail.js
@@ -32,68 +32,56 @@ class ALireDetail extends Component {
         return (
             <div>
                 <Header />
-                    <div className="body body_detail">
-                        <Link to="/infosMeteo" className="btn_titre">
-                            <div className="btn_titre_logo">
-                                <div className="btn_titre_logo_image">
-                                NORMANDIE
-                                </div>
-                                <div className="btn_titre_logo_fr">
-                                .fr
-                                </div>
-                            </div>
-                            <div className="btn_titre_titre">
-                                <div className="btn_titre_titre2">
-                                    les infos et la météo du jour en Normandie
-                                </div>
-                            </div>
-                        </Link>
-                        <div className="detail_article">
-                            <div className="detail_article_image">
-                                <img src={require("../../img/image.jpg")} alt="image" className="view"/>
-                            </div>
-                            <div className="detail_article_texte">
-                                <div className="detail_article_titre">
-                                    <h2 className="detail_article_titre2">{aLire[this.props.match.params.id].title}</h2>
-                                </div>
-                                <div clasName="detail_article_content">
-                                    <p className="detail_article_content2">{aLire[this.props.match.params.id].content}</p>
-                                </div>
-                            </div>
-                            <div className="btns">
-                                <div className="btn_base">
-                                    <div className="disc fav">
-                                        <div className="btn_base2">
-                                            <div className="btn_icon_star">
-                                                <img src={require("../../img/star.png")} alt="star" className="btn_size_icon_star"/>
-                                            </div>
-                                        </div>
+                    <div className="body">
+                        <div className="btn_titre_detail">
+                            <Link to="/infosMeteo" className="btn_titre">
+                                <div className="btn_titre_logo">
+                                    <div className="btn_titre_logo_image">
+                                    NORMANDIE
+                                    </div>
+                                    <div className="btn_titre_logo_fr">
+                                    .fr
                                     </div>
                                 </div>
-                                <div className="btn_base">
-                                    <div className="disc toread">
-                                        <div className="btn_base2">
-                                            <div className="btn_icon_read">
-                                                <img src={require("../../img/read.png")} alt="read" className="btn_size_icon_read"/>
-                                            </div>
-                                        </div>
+                                <div className="btn_titre_titre">
+                                    <div className="btn_titre_titre2">
+                                        les infos et la météo du jour en Normandie
                                     </div>
                                 </div>
-                                <div className="btn_base">
-                                    <div className="disc stop">
-                                        <div className="btn_base2">
-                                            <div className="btn_icon_read">
-                                                <img src={require("../../img/read.png")} alt="read" className="btn_size_icon_read"/>
-                                            </div>
-                                        </div>
+                            </Link>
+                        </div>
+                        <div className="detail_body_article">
+                            <div className="detail_article">
+                                <div className="detail_article_image">
+                                    <img src={require("../../img/image.jpg")} alt="image" className="view"/>
+                                </div>
+                                <div className="detail_article_texte">
+                                    <div className="detail_article_titre">
+                                        <h2 className="detail_article_titre2">{aLire[this.props.match.params.id].title}</h2>
+                                    </div>
+                                    <div clasName="detail_article_content">
+                                        <p className="detail_article_content2">{aLire[this.props.match.params.id].content}</p>
                                     </div>
                                 </div>
-                                <div className="btn_base">
-                                    <div className="disc pause">
-                                        <div className="btn_base2">
-                                            <div className="btn_icon_read">
-                                                <img src={require("../../img/read.png")} alt="read" className="btn_size_icon_read"/>
-                                            </div>
+                                <div className="btns">
+                                    <div className="btns_base">
+                                        <div className="btns-1">
+                                            <img src={require("../../img/rond.png")} alt="rond" className="btns-img"/>
+                                        </div>
+                                    </div>
+                                    <div className="btns_base">
+                                        <div className="btns-2">
+                                            <img src={require("../../img/rond.png")} alt="rond" className="btns-img"/>
+                                        </div>
+                                    </div>
+                                    <div className="btns_base">
+                                        <div className="btns-2">
+                                            <img src={require("../../img/rond.png")} alt="rond" className="btns-img"/>
+                                        </div>
+                                    </div>
+                                    <div className="btns_base">
+                                        <div className="btns-3">
+                                            <img src={require("../../img/rond.png")} alt="rond" className="btns-img"/>
                                         </div>
                                     </div>
                                 </div>
