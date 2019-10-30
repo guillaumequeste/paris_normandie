bbedd81e4bf49be417d1f0ca16a7cc7055d25764
Guillaume Queste <guillaume.queste@laposte.net>
2019-10-30 10:28:41 +0100

    profil s'abonner

--- src/components/Profil/Profil.js
+++ src/components/Profil/Profil.js
@@ -10,6 +10,40 @@ class Profil extends Component {
                 <Header />
                 <div className="body">
                     <h2>Profil</h2>
+                    <div className="bodyProfil">
+                        <div className="bodyProfil2">
+                            <div className="edit"><b>Nom</b>
+                                <span className="data"> : DUPONT</span>
+                                <div className="btn_edit">
+                                    <button className="btn_form">modifier</button>
+                                </div>
+                            </div>
+                            <div className="edit"><b>Prénom</b>
+                                <span className="data"> : Albert</span>
+                                <div className="btn_edit">
+                                    <button className="btn_form">modifier</button>
+                                </div>
+                            </div>
+                            <div className="edit"><b>Adresse</b>
+                                <span className="data"> : 25 rue de la Rose 76200 DIEPPE</span>
+                                <div className="btn_edit">
+                                    <button className="btn_form">modifier</button>
+                                </div>
+                            </div>
+                            <div className="edit"><b>Téléphone</b>
+                                <span className="data"> : 06 11 11 11 11</span>
+                                <div className="btn_edit">
+                                    <button className="btn_form">modifier</button>
+                                </div>
+                            </div>
+                            <div className="edit"><b>Adresse mail</b>
+                                <span className="data"> : azerty@azerty.fr</span>
+                                <div className="btn_edit">
+                                    <button className="btn_form">modifier</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <Footer />
             </div>
--- src/components/SAbonner/SAbonner.js
+++ src/components/SAbonner/SAbonner.js
@@ -10,6 +10,28 @@ class SAbonner extends Component {
                 <Header />
                 <div className="body">
                     <h2>S'abonner</h2>
+                    <div className="bodySabonner">
+                        <form className="bodySabonner2">
+                            <div className="edit"><b>Nom</b>
+                                <input className="input_form"/>
+                            </div>
+                            <div className="edit"><b>Prénom</b>
+                                <input className="input_form"/>
+                            </div>
+                            <div className="edit"><b>Adresse</b>
+                                <input className="input_form"/>
+                            </div>
+                            <div className="edit"><b>Téléphone</b>
+                                <input className="input_form"/>
+                            </div>
+                            <div className="edit"><b>Adresse mail</b>
+                                <input className="input_form"/>
+                            </div>
+                            <div className="btn_submit">
+                                <button>Valider</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <Footer />
             </div>
